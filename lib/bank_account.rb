1318ead080cc6fe7f4745f74b7b9065892b4abc6
Nethelbert <gilblasse@gmail.com>
2019-11-16 01:51:51 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,20 @@
 class BankAccount
+  attr_accessor :balance, :status
+  attr_reader :name
 
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+  
+  def deposit(money)
+    @balance += money
+  end
+  
+  def display_balance
+    @balance
+  end
+  
+  
 end
