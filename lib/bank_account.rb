7ad49dc5470ce65e9c36d241c8a3feb82aabbf7a
Nethelbert <gilblasse@gmail.com>
2019-11-16 02:58:40 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -13,8 +13,15 @@ class BankAccount
   end
   
   def display_balance
-    @balance
+    "Your balance is $#{@balance}."
   end
   
+  def valid?
+    @status == "open" && @balance > 0
+  end
+  
+  def close_account
+    @status = "closed"
+  end
   
 end
