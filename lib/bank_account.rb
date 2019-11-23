e3f8e2b0d5103e654af642e9f0682a3bb895b690
Ivana <ivana_pile@yahoo.com>
2019-11-22 19:20:06 -0500

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,66 @@
 class BankAccount
+  attr_reader :balance, :name, :status, :transfer
+
+    #initialize
+  def initialize(bank_account)
+    @bank_account = "bank_account"
+    @name = "Avi"
+    @balance = 1000
+    @status = "open"
+
+  end
+ 
+ #deposit
+ #can deposit money into its account
+
+ def deposit(money)
+  @balance += money
+end
+
+def status=(status)
+  @status
+end
+
+def balance=(balance)
+  @balance
+end
+#display_balance
+#can display its balance
+
+
+
+def display_balance
+puts = "Your balance is $#{self.balance}."
+end
+
+
+#valid?
+#is valid with an open status and a balance greater than 0 
+
+def valid?
+  if @status = "open" && @balance > 0 
+     true
+  
+  else
+ false
+  end
+end
+
+
+def close_account
+  @status = "closed"
+end
+def reverse_transfer
+    if valid? && receiver.balance > amount && self.status == "complete"
+      sender.deposit(self.amount)
+      receiver.withdrawal(self.amount)
+      self.status = "reversed"
+    else
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."      
+    end
+  end
+
 
 end
+
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,39 @@
+
+
 class Transfer
-  # your code here
+attr_accessor :sender, :receiver, :amount, :status
+
+def initialize(sender, receiver, amount, status="pending")
+  @sender = sender
+  @receiver = receiver
+  @amount = amount
+  @status = status
 end
+def valid?
+
+    sender.valid? && receiver.valid?
+  end
+ 
+  def execute_transaction
+    if @sender.balance > @amount && @status == "pending"
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+    else
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if @status == "complete"
+      amanda.balance = 950
+      @receiver.balance -= @amount
+      @status = "Transaction rejected. Please check your account balance."
+    end
+  end
+
+ 
+   
+  
+  end
