bedf93ef3a51e74268e3b91b9b6a4395baf6160c
Ethan Gustafson <ethan_gustafson@outlook.com>
2019-11-26 22:36:06 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,40 @@
+require 'pry'
 class BankAccount
+
+attr_accessor :balance, :status
+attr_reader :name
+
+@@bank_accounts = []
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+    @@bank_accounts << self
+  end
+
+  def self.all
+    @@bank_accounts
+  end
+
+  def deposit(deposit)
+    @balance += deposit
+  end
+
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
+
+  def valid?
+    if false
+      @status == "closed" && @balance == 0
+    else
+      @status == "open" && @balance != 0
+    end
+end
+
+  def close_account
+    @status = "closed"
+  end
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,46 @@
+require 'pry'
 class Transfer
-  # your code here
+
+attr_accessor :sender, :receiver, :status, :amount
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end
+
+  def valid?
+    @sender.valid? && @receiver.valid?
+  end
+
+  def execute_transaction
+    if valid? && @sender.balance > @amount && @status == "pending"
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+    else
+        @status = "rejected"
+        "Transaction rejected. Please check your account balance."
+      end
+    end
+
+  def reverse_transfer
+      if @status == "complete"
+        @sender.balance += @amount
+        @receiver.balance -= @amount
+        @status = "reversed"
+      end
+    end
+
 end
+
+# def back_project(project)
+  #  @backed_projects << project
+  #  project.backers << self
+#  end
+
+# def add_backer(backer)
+    #@backers << backer
+    #backer.backed_projects << self
+#  end
